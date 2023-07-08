02950288e9fd7780509522e10461aee5739143a2
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-07-08 13:34:07 +0100

    Create accessibility button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.sass";
 import Navbar from "./components/Navbar";
+import AccessibilityNewRoundedIcon from "@mui/icons-material/AccessibilityNewRounded";
 
 function App() {
 	return (
@@ -11,7 +11,14 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/news" element={<></>} />
+					<Route path="/thinking" element={<></>} />
+					<Route path="/career" element={<></>} />
+					<Route path="/contact" element={<></>} />
 				</Routes>
+				<button type="button" id="accessibility-button">
+					<AccessibilityNewRoundedIcon />
+				</button>
 			</BrowserRouter>
 		</>
 	);
