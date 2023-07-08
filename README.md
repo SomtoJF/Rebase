a7d085314c7bfc57ea94c691094a7c6d243e7bae
Francis Somtochukwu <somtofrancis5@gmail.com>
2023-07-08 11:42:14 +0100

    Convert Navbar lists to rrd navlinks

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic/Dept
+
+Home Video: https://cdn.sanity.io/files/8nn8fua5/production/4c749533161fc77c899a376ec6cd6da38973772f.mp4
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,42 @@
 import "../styles/Navbar.sass";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { NavLink } from "react-router-dom";
+
+interface links {
+	home: string;
+	about: string;
+	news: string;
+	thinking: string;
+	career: string;
+	contact: string;
+}
+
+const links: links = {
+	home: "/",
+	about: "/about",
+	news: "/news",
+	thinking: "/thinking",
+	career: "/career",
+	contact: "/contact",
+};
 
 export default function Navbar() {
 	return (
 		<nav>
 			<div id="logo">BASIC/DEPT</div>
 			<ul>
-				<li>WORK</li>
-				<li>ABOUT</li>
-				<li>NEWS</li>
-				<li>THINKING</li>
-				<li>CAREER</li>
-				<li>CONTACT</li>
+				{Object.keys(links).map((link, index) => (
+					<li>
+						<NavLink
+							to={Object.values(links)[index]}
+							className={({ isActive, isPending }) =>
+								isPending ? "pending" : isActive ? "active" : ""
+							}
+						>
+							{link.toUpperCase()}
+						</NavLink>
+					</li>
+				))}
 			</ul>
 			<div>
 				<MoreHorizIcon />
